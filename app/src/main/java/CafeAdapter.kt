35ffdbbf88eb100d8b6995acd10ab2cdb289bcac
Friliyandre <130683383+Friliyandre07@users.noreply.github.com>
2024-09-21 16:37:42 +0700

    sampe part terakhir

--- app/src/main/java/CafeAdapter.kt
+++ app/src/main/java/CafeAdapter.kt
@@ -1,3 +1,4 @@
+import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
@@ -10,13 +11,21 @@ val TABS_FIXED = listOf(
     R.string.janjijiwa_title,
     R.string.kopikenangan_title,
 )
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle, private val context: Context) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    private val descriptions = listOf(
+        R.string.starbucks_desc,
+        R.string.janjijiwa_desc,
+        R.string.kopikenangan_desc
+    )
+
     override fun getItemCount(): Int {
         return TABS_FIXED.size
     }
-    override fun createFragment(position: Int): Fragment
-    {
-        return CafeDetailFragment()
+
+    override fun createFragment(position: Int): Fragment {
+        val description = context.resources.getString(descriptions[position])
+        return CafeDetailFragment.newInstance(description)
     }
 }
--- app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -19,25 +19,30 @@ private const val TAB_CONTENT = "TAB_CONTENT"
  */
 class CafeDetailFragment : Fragment() {
     private var content: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             content = it.getString(TAB_CONTENT)
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-// Inflate the layout for this fragment
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cafe_detail, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<TextView>(R.id.content_description)
-            ?.text = content
+        view.findViewById<TextView>(R.id.content_description)?.text = content
     }
+
     companion object {
+        private const val TAB_CONTENT = "TAB_CONTENT"
+
         fun newInstance(content: String) =
             CafeDetailFragment().apply {
                 arguments = Bundle().apply {
